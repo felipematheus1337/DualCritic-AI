eace248aeb5162c257242da254480f892eeafbaf
felipematheus1337 <lipehbr@gmail.com>
2026-05-26 13:26:01 -0300

    feature: updated app.yml, created fightclubcontroller.

---
+++ src/main/java/dualcritic/ai/controller/FightClubController.java
@@ -0,0 +1,14 @@
+package dualcritic.ai.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FightClubController {
+
+    @GetMapping
+    public String index()  {
+        return "Welcome to the Fight Club! This is the home of film critiques. " +
+                "To get reviews, send a POST request to /reviews with the film name.";
+    }
+}
