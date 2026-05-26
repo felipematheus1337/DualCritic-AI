5183470342bbc4fce97f363ae8ae0a33d1f00159
felipematheus1337 <lipehbr@gmail.com>
2026-05-26 13:47:16 -0300

    feature: created controller, added openai-ai config in app.yml

--- src/main/java/dualcritic/ai/controller/FightClubController.java
+++ src/main/java/dualcritic/ai/controller/FightClubController.java
@@ -1,15 +1,33 @@
 package dualcritic.ai.controller;
 
+import dualcritic.ai.domain.FinalReviewResult;
+import dualcritic.ai.service.FightClubPromptService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class FightClubController {
+
+    private final FightClubPromptService service;
 
     @GetMapping
     public String index()  {
         return "Welcome to the Fight Club! This is the home of film critiques. " +
                 "To get reviews, send a GET request to /reviews with the film name.";
+    }
+
+    @GetMapping("/reviews")
+    @ResponseStatus(HttpStatus.CREATED)
+    public FinalReviewResult generateReview(@RequestParam(name = "film", required = false,
+            defaultValue = "Fight Club") String film) {
+
+        return service.generateFinalReview(film);
+
     }
 
 }
